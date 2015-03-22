6814ecd5c601ad35422ec77ee888a92d6e1deef9
Bressain Dinkelman <dinkelburt@gmail.com>
2015-03-21 18:31:01 -0600

    Create repo

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+// var stylus = require('gulp-stylus');
+//
+// gulp.task('css', function () {
+//   gulp.src('./public/stylesheets/*.styl')
+//       .pipe(stylus())
+//       .pipe(gulp.dest('./public/stylesheets'));
+// });
+
+gulp.task('default', ['css']);
+
+//gulp.watch('./public/**/*.styl', ['css']);
